3fe14263b552784e7e9299c6fb29290c0e9f001f
torgom2010 <88930817+torgom2010@users.noreply.github.com>
2021-09-28 22:17:57 +0400

    Add files via upload

---
+++ Torgom,xml/cmake-build-debug/CMakeFiles/Torgom_xml.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Torgom_xml.dir/main.cpp.obj"
+  "Torgom_xml.exe"
+  "Torgom_xml.exe.manifest"
+  "Torgom_xml.lib"
+  "Torgom_xml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Torgom_xml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Torgom,xml/cmake-build-debug/CMakeFiles/Torgom_xml.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Torgom_xml.
+# This may be replaced when dependencies are built.
